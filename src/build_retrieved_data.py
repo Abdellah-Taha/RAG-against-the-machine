4ebace036464ee414d95497b5356210bc6d3f3fc
Valar <abdellahzirari2005@gmail.com>
2026-09-04 00:02:03 +0100

    added lru_cache for efficiency

--- src/build_retrieved_data.py
+++ src/build_retrieved_data.py
@@ -1,12 +1,20 @@
 from data_models import MinimalSearchResults, MinimalSource, StudentSearchResults
 from typing import List
 from retrieval import retrieval
+from functools import lru_cache
 import time
+
+@lru_cache(maxsize=128)
+def cached_retrieval(query: str, max_k=50):
+    return retrieval(query, max_k)
 
 def build_retrieved_data(query: str, k: int, meta_data: List[dict]):
     minimal_search_results = MinimalSearchResults(question_id="", question=query, retrieved_sources=[])
-    results, scores = retrieval(query, k)
-    for i in range(len(results[0])):
+    results, scores = cached_retrieval(query, max_k=50)
+    
+    limit = min(k, len(results[0]))
+    
+    for i in range(limit):
         doc_idx = results[0][i]
         minimal_source = MinimalSource(
             file_path=meta_data[doc_idx]['file_path'],
@@ -14,6 +22,7 @@ def build_retrieved_data(query: str, k: int, meta_data: List[dict]):
             last_character_index=meta_data[doc_idx]['end'],
         )
         minimal_search_results.retrieved_sources.append(minimal_source)
+        
     return minimal_search_results
 
 def total_search_results(queries: List[str], k: int, meta_data: List[dict]):
