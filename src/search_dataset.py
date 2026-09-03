2162fc6ba88b9b874169ad50511ffe488ad47d46
Valar <abdellahzirari2005@gmail.com>
2026-09-03 23:52:54 +0100

    RAG DWA

--- src/search_dataset.py
+++ src/search_dataset.py
@@ -21,33 +21,43 @@ def retrieve_data_source(file_path: str):
     return [item["sources"] for item in data_set]
     
     
-def review_results(path):
-    correct_count, total = 0,0
+def review_results(path: str, k: int, meta_data: List[dict]):
+    correct_count, total = 0, 0
     data_set = retrieve_questions(path)
     source = retrieve_data_source(path)
-    k = 10
-    start_time = time.time()
-    meta_data = index_files()
-    end_time = time.time()
-    print(f"Indexing complete in {end_time - start_time:.2f} seconds. You can now use the BM25 retriever for searching.")
+
     student_search_results = total_search_results(data_set, k, meta_data)
     for i, result in enumerate(student_search_results.search_results):
-        print(f"\nQuestion {i+1}: {result.question}")
+        # print(f"\nQuestion {i+1}: {result.question}")
         
         ground_truth_paths = [s["file_path"] for s in source[i]]
         student_paths = result.retrieved_sources
         matches = [path.file_path for path in student_paths if path.file_path in ground_truth_paths]
         if matches:
-            print(f"✅ Success! Found true path(s): {matches}")
+            # print(f"✅ Success! Found true path(s): {matches}")
             correct_count += 1
             total +=1
         else:
-            print(f"❌ Missed. Expected: {ground_truth_paths[0]}")
+            # print(f"❌ Missed. Expected: {ground_truth_paths[0]}")
             total +=1
-    print(f"\n\nPercentage: {(correct_count/total)*100:.2f} % 100")
+    print(f"Total Questions: {total}")
+    print(f"total student answers: {len(student_search_results.search_results)}")
+    print(f"recall@{k}: {correct_count}/{total}  ({(correct_count/total)*100:.2f}%)")
     
 def main():
-    review_results(path_docs)
+    start_time = time.time()
+    meta_data = index_files()
+    end_time = time.time()
+    print(f"Indexing complete in {end_time - start_time:.2f} sec    onds. You can now use the BM25 retriever for searching.")
+    review_results(path_docs, k=1, meta_data=meta_data)
+    review_results(path_docs, k=2, meta_data=meta_data)
+    review_results(path_docs, k=5, meta_data=meta_data)
+    review_results(path_docs, k=10, meta_data=meta_data)
+    print("==================================================")
+    review_results(path_code, k=1, meta_data=meta_data)
+    review_results(path_code, k=2, meta_data=meta_data)
+    review_results(path_code, k=5, meta_data=meta_data)
+    review_results(path_code, k=10, meta_data=meta_data)
 
 if __name__ == "__main__":
     main()
